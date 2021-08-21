cd76c380b50f2907ee91e52c8d314e3581675fe5
fatih <fatihayoz20800@gmail.com>
2021-08-22 00:49:35 +0200

    fetch data from NASA API

---
+++ index.js
@@ -0,0 +1,22 @@
+const url = "https://api.nasa.gov/planetary/apod?api_key=";
+
+const apiKey = config.NASA_API_KEY;
+
+const fetchNASAData = async () => {
+  try {
+    const response = await fetch(`${url}${apiKey}`);
+    const data = await response.json();
+    displayData(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayData = (data) => {
+  document.querySelector(".title").textContent = data.title;
+  document.querySelector(".date").textContent = data.date;
+  document.querySelector(".picture").src = data.url;
+  document.querySelector(".explanation").textContent = data.explanation;
+};
+
+fetchNASAData();
